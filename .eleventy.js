05f9d7e1fe739a05c809a84104060a0b4ce73c0d
iamdesh <deshrajgocher2012@gmail.com>
2025-08-28 11:03:09 -0400

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,22 +1,19 @@
 module.exports = function(eleventyConfig) {
-  // copy admin and images to output
-  eleventyConfig.addPassthroughCopy("admin");
-  eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy({ "static": "/" }); // images/uploads etc.
 
-  // human-friendly date filter
-  eleventyConfig.addFilter("readableDate", (dateObj) =>
-    new Intl.DateTimeFormat("en-US", { dateStyle: "medium" }).format(dateObj)
-  );
-
-  // blog collection from /posts/*.md
-  eleventyConfig.addCollection("posts", (collectionApi) =>
-    collectionApi.getFilteredByGlob("./posts/*.md").reverse()
-  );
+  eleventyConfig.addCollection("posts", (collectionApi) => {
+    return collectionApi.getFilteredByGlob("src/posts/*.{md,njk}");
+  });
 
   return {
-    dir: { input: "src", includes: "_includes", output: "_site" },
-    markdownTemplateEngine: "njk",
+    dir: {
+      input: "src",
+      includes: "_includes",
+      data: "_data",
+      output: "_site"
+    },
     htmlTemplateEngine: "njk",
-    templateFormats: ["md","njk","html"]
+    markdownTemplateEngine: "njk",
+    templateFormats: ["html", "njk", "md"]
   };
 };
